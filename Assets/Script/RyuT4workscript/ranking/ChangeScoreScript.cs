e88f609f573360af7d4e5e2b187393691304da8e
Ryuta Suzumura <ryu.300589@gmail.com>
2022-06-21 14:36:45 +0900

    Revert "ランキング表示完了。入れ替えは未実装。」"
    This reverts commit b8c21cce9524d683f02ab282dbc3a68146332f8f.

--- Assets/Script/RyuT4workscript/ranking/ChangeScoreScript.cs
+++ Assets/Script/RyuT4workscript/ranking/ChangeScoreScript.cs
@@ -13,8 +13,7 @@ public class ChangeScoreScript : MonoBehaviour
     public Text Rank5;
     int score;
     public Text demo;
-    public int[] RankSc = new int[] { 0, 0, 0, 0, 0 };
-    string[] ranking = { "Rank1", "Rank2", "Rank3", "Rank4", "Rank5" };
+    public int[] RankSc = { };
 
     public void Start()
     {
@@ -25,23 +24,20 @@ public class ChangeScoreScript : MonoBehaviour
         {
             if(score >= RankSc[i])
             {
-                var change = RankSc[i];
+                int change = RankSc[i];
                 RankSc[i] = score;
                 score = change;
             }
         }
 
-        for (int i = 0; i < 5; i++)
-        {
-            PlayerPrefs.SetInt(ranking[i], RankSc[i]);
-            PlayerPrefs.Save();
-        }
-
-        Rank1.text = string.Format("1位：{0}", PlayerPrefs.GetInt("Rank1"));
-        Rank2.text = string.Format("2位：{0}", PlayerPrefs.GetInt("Rank2"));
-        Rank3.text = string.Format("3位：{0}", PlayerPrefs.GetInt("Rank3"));
-        Rank4.text = string.Format("4位：{0}", PlayerPrefs.GetInt("Rank4"));
-        Rank5.text = string.Format("5位：{0}", PlayerPrefs.GetInt("Rank5"));
     }
 
+    public void RankOutput()
+    {
+        Rank1.text = string.Format("1位：{0}", RankSc[0]);
+        Rank2.text = string.Format("2位：{0}", RankSc[1]);
+        Rank3.text = string.Format("3位：{0}", RankSc[2]);
+        Rank4.text = string.Format("4位：{0}", RankSc[3]);
+        Rank5.text = string.Format("5位：{0}", RankSc[4]);
+    }
 }
