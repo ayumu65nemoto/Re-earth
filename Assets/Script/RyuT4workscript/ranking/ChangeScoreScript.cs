b1b509d1cf33851bbf38b9e2fcee3ededfb5bf67
Ryuta Suzumura <ryu.300589@gmail.com>
2022-06-21 15:21:23 +0900

    ランキング・オプションシーンにタイトルに戻るボタンを作成。ランキング入れ替え実装。

--- Assets/Script/RyuT4workscript/ranking/ChangeScoreScript.cs
+++ Assets/Script/RyuT4workscript/ranking/ChangeScoreScript.cs
@@ -13,7 +13,8 @@ public class ChangeScoreScript : MonoBehaviour
     public Text Rank5;
     int score;
     public Text demo;
-    public int[] RankSc = { };
+    public int[] RankSc = new int[] { 0, 0, 0, 0, 0 };
+    string[] ranking = { "Rank1", "Rank2", "Rank3", "Rank4", "Rank5" };
 
     public void Start()
     {
@@ -22,22 +23,30 @@ public class ChangeScoreScript : MonoBehaviour
 
         for (int i = 0; i < 5; i++)
         {
+            RankSc[i] = PlayerPrefs.GetInt(ranking[i], 0);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
             if(score >= RankSc[i])
             {
-                int change = RankSc[i];
+                var change = RankSc[i];
                 RankSc[i] = score;
                 score = change;
             }
         }
 
+        for (int i = 0; i < 5; i++)
+        {
+            PlayerPrefs.SetInt(ranking[i], RankSc[i]);
+            PlayerPrefs.Save();
+        }
+
+        Rank1.text = string.Format("1位：{0}", PlayerPrefs.GetInt("Rank1"));
+        Rank2.text = string.Format("2位：{0}", PlayerPrefs.GetInt("Rank2"));
+        Rank3.text = string.Format("3位：{0}", PlayerPrefs.GetInt("Rank3"));
+        Rank4.text = string.Format("4位：{0}", PlayerPrefs.GetInt("Rank4"));
+        Rank5.text = string.Format("5位：{0}", PlayerPrefs.GetInt("Rank5"));
     }
 
-    public void RankOutput()
-    {
-        Rank1.text = string.Format("1位：{0}", RankSc[0]);
-        Rank2.text = string.Format("2位：{0}", RankSc[1]);
-        Rank3.text = string.Format("3位：{0}", RankSc[2]);
-        Rank4.text = string.Format("4位：{0}", RankSc[3]);
-        Rank5.text = string.Format("5位：{0}", RankSc[4]);
-    }
 }
