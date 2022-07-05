c3931e26be3e25e65c288fef5ec2bc5c34932e4c
Nakajima <aki.metalslime@gmail.com>
2022-07-05 17:44:04 +0900

    スタート画面用録画カメラ設定

--- Assets/shou/camera_war.cs
+++ Assets/shou/camera_war.cs
@@ -4,30 +4,49 @@ using UnityEngine;
 
 public class camera_war : MonoBehaviour
 {
-    
+    // 値を変化させる秒数です。
+    float animTime = 3.0f;
 
-    public float timeOut;
-    private float timeElapsed;
-    private float startTime;
+    // アニメーションの終了予定時刻です。
+    float finishTime;
 
-    // Start is called before the first frame update
+    // アニメーションの初期位置です。
+    Vector3 initPos;
+
+    // アニメーションの終了位置です。
+    Vector3 targetPos;
+
+    // 注視したいオブジェクト
+    public GameObject targetObject;
+
     void Start()
     {
-        
+        // アニメーションを開始できるように情報をセットします。
+        finishTime = Time.time + animTime;
+        initPos = gameObject.transform.position;
+        targetPos = new Vector3(initPos.x + 30.0f, initPos.y+20.0f, initPos.z-20.0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //timeElapsed += Time.deltaTime;
 
-        //if (timeElapsed >= timeOut >= startTime)
-        //{
-            // Do anything
-        //    startTime = 10.0f;
-        //   timeElapsed = 20.0f;
+        if (Time.time < finishTime)
+        {
+        // アニメーションの残り時間を取得します。
+            float restTime = finishTime - Time.time;
 
-            transform.position -= transform.forward;
-        //}
+        // アニメーションの経過時間を取得します。
+            float elapsedTime = Mathf.Clamp01((finishTime - restTime) / animTime);
+
+        // 時間に応じた現在の位置を取得してセットします。
+            Vector3 pos = Vector3.Lerp(initPos, targetPos, elapsedTime);
+            gameObject.transform.position = pos;
+
+        }
+        if (Time.time > finishTime)
+        {
+            //回転を与える
+            transform.RotateAround(targetObject.transform.position, new Vector3(0, 1, 0), 0.3f);
+        }
     }
 }
