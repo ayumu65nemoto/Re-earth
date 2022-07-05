c9e68702292998f8c2cfb70cac7a2c32738139db
Ryuta Suzumura <ryu.300589@gmail.com>
2022-07-05 15:22:11 +0900

    ゲームパッドの操作デモ版の作成。

--- Assets/Script/RyuT4workscript/returnTitleScript.cs
+++ Assets/Script/RyuT4workscript/returnTitleScript.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class returnTitleScript : MonoBehaviour
 {
+    public Button FirstSelectButton;
+
+    void Start()
+    {
+        FirstSelectButton.Select();
+    }
     // Start is called before the first frame update
     public void OnClickStartButton()
     {
