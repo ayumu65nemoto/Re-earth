90b8a008727f66b90f152a0489518ddb0f77fedf
Ryuta Suzumura <ryu.300589@gmail.com>
2022-07-25 23:21:30 +0900

    ゲームシーンに入る直前にカウントダウンを実装。（モードセレクトの箇所でボタンを押すとなる）これでいきなりゲームが開始されることはない。

--- Assets/Script/RyuT4workscript/modeScript1.cs
+++ Assets/Script/RyuT4workscript/modeScript1.cs
@@ -1,14 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class modeScript1 : MonoBehaviour
 {
+    public float counttime;
+    public bool startkey;
+    public bool counter;
+    public Text timerText;
+    public GameObject black1;
     // Start is called before the first frame update
-    public void OnClickStartButton()
+
+    private void Start()
     {
-        SceneManager.LoadScene("game1");
+        counter = false;
+        startkey = false;
+        counttime = 3f;
+        black1.SetActive(false);
     }
+
+    private void Update()
+    {
+        if(counter == true)
+        {
+            counttime -= Time.deltaTime;
+            black1.SetActive(true);
+            timerText.text = counttime.ToString("F0");
+            if (counttime < 0)
+            {
+                startkey = true;
+            }
+            if (startkey == true)
+            {
+                SceneManager.LoadScene("game1");
+            }
+        }
+
+
+
+    }
+
+
+    void OnClickStartButton()
+    {
+        counter = true;
+    }
+
+
+
 
 }
