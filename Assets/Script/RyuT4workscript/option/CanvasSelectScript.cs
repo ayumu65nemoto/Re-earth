5c082c1ac8e07691c23bf41923d461d0546f9fdb
Ryuta Suzumura <ryu.300589@gmail.com>
2022-10-17 12:30:52 +0900

    オプションのUIなどを分かりやすく変更

--- Assets/Script/RyuT4workscript/option/CanvasSelectScript.cs
+++ Assets/Script/RyuT4workscript/option/CanvasSelectScript.cs
@@ -5,43 +5,99 @@ using UnityEngine.UI;
 
 public class CanvasSelectScript : MonoBehaviour
 {
+    public GameObject Panel0;
     public GameObject Panel1;
     public GameObject Panel2;
-    public GameObject Panel3;
     public Button select1;
     public Button select2;
-    public Button select3;
     public Button FirstSelectButton;
+    public int changekey;
 
     // Start is called before the first frame update
     void Start()
     {
+        Panel0.SetActive(true);
         Panel1.SetActive(false);
         Panel2.SetActive(false);
-        Panel3.SetActive(true);
         FirstSelectButton.Select();
+        changekey = 0;
     }
 
     // Update is called once per frame
 
     public void TappedButton1()
     {
-        Panel1.SetActive(false);
-        Panel2.SetActive(true);
-        Panel3.SetActive(false);
+        changekey += 1;
+
+        if(changekey == -1)
+        {
+            changekey = 2;
+        }
+
+        if(changekey == 3)
+        {
+            changekey = 0;
+        }
+
+        if(changekey == 0)
+        {
+            Panel0.SetActive(true);
+            Panel1.SetActive(false);
+            Panel2.SetActive(false);
+        }
+
+        if(changekey == 1)
+        {
+            Panel0.SetActive(false);
+            Panel1.SetActive(true);
+            Panel2.SetActive(false);
+        }
+
+        if(changekey == 2)
+        {
+            Panel0.SetActive(false);
+            Panel1.SetActive(false);
+            Panel2.SetActive(true);
+        }
+
+
+
     }
 
     public void TappedButton2()
     {
-        Panel1.SetActive(false);
-        Panel2.SetActive(false);
-        Panel3.SetActive(true);
+        changekey -= 1;
+
+        if (changekey == -1)
+        {
+            changekey = 2;
+        }
+
+        if (changekey == 3)
+        {
+            changekey = 0;
+        }
+
+        if (changekey == 0)
+        {
+            Panel0.SetActive(true);
+            Panel1.SetActive(false);
+            Panel2.SetActive(false);
+        }
+
+        if (changekey == 1)
+        {
+            Panel0.SetActive(false);
+            Panel1.SetActive(true);
+            Panel2.SetActive(false);
+        }
+
+        if (changekey == 2)
+        {
+            Panel0.SetActive(false);
+            Panel1.SetActive(false);
+            Panel2.SetActive(true);
+        }
     }
 
-    public void TappedButton3()
-    {
-        Panel1.SetActive(true);
-        Panel2.SetActive(false);
-        Panel3.SetActive(false);
-    }
 }
