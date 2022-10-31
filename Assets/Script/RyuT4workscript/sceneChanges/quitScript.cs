ecbe8563f2075a8f47dc8824b45c5219ec171aab
Ryuta Suzumura <ryu.300589@gmail.com>
2022-10-31 15:53:31 +0900

    タイトルシーンに強制終了ボタン追加。また、クレジットシーンとオプションシーン追加（旧オプションシーンはガイドシーンに変更）

--- Assets/Script/RyuT4workscript/sceneChanges/quitScript.cs
+++ Assets/Script/RyuT4workscript/sceneChanges/quitScript.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SettingScript : MonoBehaviour
+public class quitScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public void OnClickStartButton()
     {
-        SceneManager.LoadScene("options");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#else
+        Application.Quit();
+#endif
     }
+
 }
