e4047650742f92ea65e79968fc5e2d91a18f311b
Ryuta Suzumura <ryu.300589@gmail.com>
2022-11-21 15:52:21 +0900

    シーン遷移処理

---
+++ Assets/Script/RyuT4workscript/scoresceneScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scoresceneScript : MonoBehaviour
+{
+
+    private float scenetime = 9.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scenetime -= Time.deltaTime;
+        if (scenetime <= 0)
+        {
+            SceneManager.LoadScene("ranking");
+            scenetime = 9.0f;
+        }
+    }
+}
